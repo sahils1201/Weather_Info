4e48a04d8c7cec875c7c5e04951ce94be8875b17
Sahil <sahilsable737@gmail.com>
2024-01-04 10:40:30 +0530

    Changes

---
+++ api.py
@@ -0,0 +1,1 @@
+apikey="ea8a0beff64c08f2e9d51ee57f543e3a"
--- api_key.py
+++ api_key.py
@@ -1,8 +1,9 @@
 import datetime as dt
 import requests
+from api import apikey
 
 baseurl="http://api.openweathermap.org/data/2.5/weather?"
-api_key="ea8a0beff64c08f2e9d51ee57f543e3a"
+# api_key="ea8a0beff64c08f2e9d51ee57f543e3a"
 city=input('Enter City: ')
 
 def kel_to_cel_fahren(kelvin):
@@ -10,7 +11,7 @@ def kel_to_cel_fahren(kelvin):
     fahrenheit=celsius*(9/5)+32
     return celsius,fahrenheit
 
-url= baseurl + 'appid=' + api_key + '&q=' + city
+url= baseurl + 'appid=' + apikey + '&q=' + city
 response=requests.get(url).json()
 # print(response)
 
